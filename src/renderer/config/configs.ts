55d3e4555a19b712575bd2fa2fcc07a965e4f728
Kim <jin.qian@ximalaya.com>
2021-12-13 19:54:11 +0800

    init

---
+++ src/renderer/config/configs.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: Kim
+ * @Date: 2021-12-13 19:37:55
+ * @LastEditTime: 2021-12-13 19:53:33
+ * @LastEditors: Kim
+ * @Description:
+ * @FilePath: /ximaToolkit/src/renderer/config/configs.ts
+ */
+
+export default {
+	title: 'ximaToolkit',
+	layout: {
+		siderWidth: 208,
+	},
+};
---
+++ src/renderer/config/routes.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: Kim
+ * @Date: 2021-12-13 19:40:44
+ * @LastEditTime: 2021-12-13 19:53:42
+ * @LastEditors: Kim
+ * @Description:
+ * @FilePath: /ximaToolkit/src/renderer/config/routes.ts
+ */
+export default [
+	{
+		path: '/packages',
+		component: '',
+	},
+];
